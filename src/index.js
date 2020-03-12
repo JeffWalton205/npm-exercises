c05f26423957197e111cdb0237ffc8391b38524a
Jeff Walton <jeff.walton205@gmail.com>
2020-03-12 15:13:56 -0500

    NPM exercise

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const $ = require("jquery");
+let name = 'Jeff';
+const sayHello = () => 'Hello, ' + name + '!';
+console.log(sayHello());
+
+// $(".red").click(function (){
+//     $(this).css("color", "red");
+// });
+
+$('.red').css('background','red');
+
+$('.blue').css('color','blue');
+
